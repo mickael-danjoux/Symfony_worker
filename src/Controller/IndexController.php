19e5180a8acfa80964e2ae6325a046c258d69b66
Mickaël DANJOUX <mickael@sware.fr>
2024-03-21 14:44:55 +0100

    Display Failed queue

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\ContactType;
 use App\Message\LongActionMessage;
+use App\Repository\FailedMessageRepository;
 use App\Services\ContactService;
 use App\Services\LongActionService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,7 +13,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Messenger\MessageBusInterface;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 class IndexController extends AbstractController
@@ -20,7 +20,8 @@ class IndexController extends AbstractController
     public function __construct(
         private readonly ContactService $contactService,
         private readonly LongActionService $longActionService,
-        private readonly MessageBusInterface $bus
+        private readonly MessageBusInterface $bus,
+        private readonly FailedMessageRepository $failedMessageRepository
     ) {
     }
 
@@ -35,6 +36,7 @@ class IndexController extends AbstractController
 
         return $this->render('index.html.twig', [
             'form' => $form->createView(),
+            'failedMessages' => $this->failedMessageRepository->findAll()
         ]);
     }
 
@@ -54,6 +56,34 @@ class IndexController extends AbstractController
         $this->bus->dispatch(new LongActionMessage($error));
         $this->addFlash('success', 'Result will be send by email.');
         return $this->redirectToRoute('app_index');
+    }
+
+    #[Route('/remove-message/{id}', name: 'app_remove_message')]
+    public function removeMessage(int $id): RedirectResponse
+    {
+        $this->failedMessageRepository->delete($id);
+        $this->addFlash('success', 'Success');
+        return $this->redirectToRoute('app_index');
+
+    }
+
+
+    #[Route('/retry-message/{id}', name: 'app_retry_message')]
+    public function retryMessage(int $id): RedirectResponse
+    {
+        $message = $this->failedMessageRepository->find($id)->getMessage();
+        if($message){
+            // Resend Message
+            //$this->bus->dispatch($message);
+
+            // For the example, new custom message is created to simulate success
+            $this->bus->dispatch(new LongActionMessage(false));
+
+            $this->failedMessageRepository->delete($id);
+        }
+        $this->addFlash('success', 'Success');
+        return $this->redirectToRoute('app_index');
+
     }
 
 }
